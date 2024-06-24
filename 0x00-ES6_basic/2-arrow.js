a61fb0b02874fda0bf2735fcc90fce44668f5a07
bryansine <bryansine1738@gmail.com>
2024-06-24 04:46:32 -0700

    updates

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+/* arrow functions */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
